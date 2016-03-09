99ac62d5ec5926a489500e8897b6d46bb2155212
violette <violette.roche-montane@forgerock.com>
2016-03-09 15:28:09 +0100

    Added gitPreparePush script
    Used to synchronize your local git repository with upstream changes.

---
+++ README.md
@@ -0,0 +1,5 @@
+bash-utility-scripts
+======
+
+This repository contains scripts to help you with git and others...  
+
---
+++ git_prepare_push.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Use to synchronize your local repository with upstream changes.
+# See https://www.atlassian.com/git/tutorials/syncing/git-pull
+# by vrom 2015-2016.
+current_branch=$(git symbolic-ref --short HEAD)
+# This above, save your current branch name
+echo -e "------------------------------"
+echo -e " >>>> REBASING YOUR WORK <<<<"
+echo -e "------------------------------"
+git checkout master
+echo -e " \n>> git pull on master... "
+git pull --rebase origin master
+# git pull --rebase origin master
+echo -e " \n>>"&&git checkout $current_branch&&echo -e ">>"
+echo -e " \n>> Rebasing $current_branch and replay your work on top of master "
+git rebase master
+echo -e "------------------------------"
+echo -e " >>>>       READY!       >>>>"
+echo -e "------------------------------"
