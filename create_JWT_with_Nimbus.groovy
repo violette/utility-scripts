0abc11400a2e701a4648572b908b5b8d42057819
violette <violette@users.noreply.github.com>
2018-07-04 12:52:03 +0200

    Create create_JWT_with_Nimbus.groovy
    Generates a new JWT. Could be launched with `groovy <script name>`

---
+++ create_JWT_with_Nimbus.groovy
@@ -0,0 +1,23 @@
+@Grapes(
+    @Grab(group = 'com.nimbusds', module = 'nimbus-jose-jwt', version = '4.41.1')
+)
+
+import com.nimbusds.jose.PlainHeader
+import com.nimbusds.jwt.JWTClaimsSet
+import com.nimbusds.jwt.PlainJWT
+
+import static java.util.Collections.singletonMap
+
+/**
+ * Generates an UNSIGNED JWT.
+ * @author violette - 2018
+ */
+def claimsSet = new JWTClaimsSet.Builder()
+        .issueTime(new Date(1502382118))
+        .claim("custom1", "value")
+        .build();
+def header = new PlainHeader.Builder()
+        .customParams(singletonMap("customHeader", "value"))
+        .build();
+def jwsObject = new PlainJWT(header, claimsSet);
+println("JWT >>>> " + jwsObject.serialize());
